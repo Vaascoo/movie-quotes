3e0532064ffb2b5c89b1df5fe6297bedf705916b
Vaascoo <vcvasco1@gmail.com>
2021-09-05 23:36:06 +0100

    Update quote_printer.py

--- quote_printer.py
+++ quote_printer.py
@@ -4,5 +4,5 @@ import json, random, os
 
 if __name__ == "__main__":
     lst = json.load(open(os.environ["HOME"] + "/.local/bin/" + "quotes.json"))
-    quote : dict = lst[random.randint(0, len(lst))]
+    quote : dict = lst[random.randint(0, len(lst) - 1)]
     print(f"{quote['Quote']} -- {quote['Character']} ({quote['Interpreter']}) in \"{quote['Film']}\", {quote['Year']}")
